672e9b1716968f3579aeeb3d3ba4b109bf392465
zhiqiang.liu <837500869@qq.com>
2016-04-15 20:13:03 +0800

    3.0.0

--- azkaban-webserver/src/main/java/azkaban/webapp/servlet/ProjectServlet.java
+++ azkaban-webserver/src/main/java/azkaban/webapp/servlet/ProjectServlet.java
@@ -39,108 +39,105 @@ import azkaban.webapp.AzkabanWebServer;
  * The main page
  */
 public class ProjectServlet extends LoginAbstractAzkabanServlet {
-  private static final Logger logger = Logger.getLogger(ProjectServlet.class
-      .getName());
-  private static final String LOCKDOWN_CREATE_PROJECTS_KEY =
-      "lockdown.create.projects";
-  private static final long serialVersionUID = -1;
+	private static final Logger logger = Logger.getLogger(ProjectServlet.class.getName());
+	private static final String LOCKDOWN_CREATE_PROJECTS_KEY = "lockdown.create.projects";
+	private static final long serialVersionUID = -1;
 
-  private UserManager userManager;
+	private UserManager userManager;
 
-  private boolean lockdownCreateProjects = false;
+	private boolean lockdownCreateProjects = false;
 
-  @Override
-  public void init(ServletConfig config) throws ServletException {
-    super.init(config);
-    AzkabanWebServer server = (AzkabanWebServer) getApplication();
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		AzkabanWebServer server = (AzkabanWebServer) getApplication();
 
-    userManager = server.getUserManager();
-    lockdownCreateProjects =
-        server.getServerProps().getBoolean(LOCKDOWN_CREATE_PROJECTS_KEY, false);
-    if (lockdownCreateProjects) {
-      logger.info("Creation of projects is locked down");
-    }
-  }
+		userManager = server.getUserManager();
+		lockdownCreateProjects = server.getServerProps().getBoolean(LOCKDOWN_CREATE_PROJECTS_KEY, false);
+		if (lockdownCreateProjects) {
+			logger.info("Creation of projects is locked down");
+		}
+	}
 
-  @Override
-  protected void handleGet(HttpServletRequest req, HttpServletResponse resp,
-      Session session) throws ServletException, IOException {
-    if (hasParam(req, "doaction")) {
-      if (getParam(req, "doaction").equals("search")) {
-        String searchTerm = getParam(req, "searchterm");
-        if (!searchTerm.equals("") && !searchTerm.equals(".*")) {
-          handleFilter(req, resp, session, searchTerm);
-          return;
-        }
-      }
-    }
+	@Override
+	protected void handleGet(HttpServletRequest req, HttpServletResponse resp, Session session)
+			throws ServletException, IOException {
+		if (hasParam(req, "doaction")) {
+			if (getParam(req, "doaction").equals("search")) {
+				String searchTerm = getParam(req, "searchterm");
+				if (!searchTerm.equals("") && !searchTerm.equals(".*")) {
+					handleFilter(req, resp, session, searchTerm);
+					return;
+				}
+			}
+		}
 
-    User user = session.getUser();
+		User user = session.getUser();
 
-    ProjectManager manager =
-        ((AzkabanWebServer) getApplication()).getProjectManager();
-    Page page =
-        newPage(req, resp, session, "azkaban/webapp/servlet/velocity/index.vm");
+		ProjectManager manager = ((AzkabanWebServer) getApplication()).getProjectManager();
+		Page page = newPage(req, resp, session, "azkaban/webapp/servlet/velocity/index.vm");
 
-    if (lockdownCreateProjects && !hasPermissionToCreateProject(user)) {
-      page.add("hideCreateProject", true);
-    }
+		if (lockdownCreateProjects && !hasPermissionToCreateProject(user)) {
+			page.add("hideCreateProject", true);
+		}
 
-    if (hasParam(req, "all")) {
-      List<Project> projects = manager.getProjects();
-      page.add("viewProjects", "all");
-      page.add("projects", projects);
-    } else if (hasParam(req, "group")) {
-      List<Project> projects = manager.getGroupProjects(user);
-      page.add("viewProjects", "group");
-      page.add("projects", projects);
-    } else {
-      List<Project> projects = manager.getUserProjects(user);
-      page.add("viewProjects", "personal");
-      page.add("projects", projects);
-    }
+		if (hasParam(req, "all")) {
+			logger.debug("ProjectServlet all");
+			List<Project> projects = manager.getProjects();
+			page.add("viewProjects", "all");
+			page.add("projects", projects);
+		} else if (hasParam(req, "group")) {
+			logger.debug("ProjectServlet group");
+			List<Project> projects = manager.getGroupProjects(user);
+			page.add("viewProjects", "group");
+			page.add("projects", projects);
+		} else {
+			logger.debug("ProjectServlet else");
+			List<Project> projects = manager.getUserProjects(user);
+			for (Project project : projects) {
+				logger.debug("List<Project> projects---" + project.getId() + "-" + project.getName());
+			}
+			page.add("viewProjects", "personal");
+			page.add("projects", projects);
+		}
 
-    page.render();
-  }
+		page.render();
+	}
 
-  private void handleFilter(HttpServletRequest req, HttpServletResponse resp,
-      Session session, String searchTerm) {
-    User user = session.getUser();
-    ProjectManager manager =
-        ((AzkabanWebServer) getApplication()).getProjectManager();
-    Page page =
-        newPage(req, resp, session, "azkaban/webapp/servlet/velocity/index.vm");
-    if (hasParam(req, "all")) {
-      // do nothing special if one asks for 'ALL' projects
-      List<Project> projects = manager.getProjectsByRegex(searchTerm);
-      page.add("allProjects", "");
-      page.add("projects", projects);
-      page.add("search_term", searchTerm);
-    } else {
-      List<Project> projects = manager.getUserProjectsByRegex(user, searchTerm);
-      page.add("projects", projects);
-      page.add("search_term", searchTerm);
-    }
+	private void handleFilter(HttpServletRequest req, HttpServletResponse resp, Session session, String searchTerm) {
+		User user = session.getUser();
+		ProjectManager manager = ((AzkabanWebServer) getApplication()).getProjectManager();
+		Page page = newPage(req, resp, session, "azkaban/webapp/servlet/velocity/index.vm");
+		if (hasParam(req, "all")) {
+			// do nothing special if one asks for 'ALL' projects
+			List<Project> projects = manager.getProjectsByRegex(searchTerm);
+			page.add("allProjects", "");
+			page.add("projects", projects);
+			page.add("search_term", searchTerm);
+		} else {
+			List<Project> projects = manager.getUserProjectsByRegex(user, searchTerm);
+			page.add("projects", projects);
+			page.add("search_term", searchTerm);
+		}
 
-    page.render();
-  }
+		page.render();
+	}
 
-  @Override
-  protected void handlePost(HttpServletRequest req, HttpServletResponse resp,
-      Session session) throws ServletException, IOException {
-    // TODO Auto-generated method stub
-  }
+	@Override
+	protected void handlePost(HttpServletRequest req, HttpServletResponse resp, Session session)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
 
-  private boolean hasPermissionToCreateProject(User user) {
-    for (String roleName : user.getRoles()) {
-      Role role = userManager.getRole(roleName);
-      Permission perm = role.getPermission();
-      if (perm.isPermissionSet(Permission.Type.ADMIN)
-          || perm.isPermissionSet(Permission.Type.CREATEPROJECTS)) {
-        return true;
-      }
-    }
+	private boolean hasPermissionToCreateProject(User user) {
+		for (String roleName : user.getRoles()) {
+			Role role = userManager.getRole(roleName);
+			Permission perm = role.getPermission();
+			if (perm.isPermissionSet(Permission.Type.ADMIN) || perm.isPermissionSet(Permission.Type.CREATEPROJECTS)) {
+				return true;
+			}
+		}
 
-    return false;
-  }
+		return false;
+	}
 }
